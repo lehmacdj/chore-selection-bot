bac9e5809c908b0c5dab8dd90a8d41c1d9adbc19
Devin Lehmacher <lehmacdj@gmail.com>
2019-01-27 14:52:08 -0500

    echo body as well

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,8 @@ module Main where
 
 import Lib
 
+import Control.Monad.IO.Class
+
 import Web.Scotty
 
 import Data.String (fromString)
@@ -11,5 +13,17 @@ import Data.String (fromString)
 import Data.Monoid (mconcat)
 
 main = scotty 80 $ do
-    post "/select" (request >>= html . fromString . show)
-    post "/help" (request >>= html . fromString . show)
+    post "/select" $ do
+        req <- request
+        bod <- body
+        let req' = show req
+        let bod' = show bod
+        let resp = req' ++ "\n" ++ bod'
+        html $ fromString resp
+    post "/help" $ do
+        req <- request
+        bod <- body
+        let req' = show req
+        let bod' = show bod
+        let resp = req' ++ "\n" ++ bod'
+        html $ fromString resp
