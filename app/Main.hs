7b2897197df45caf2dd353bc8bb8b0b2a19f1211
Devin Lehmacher <lehmacdj@gmail.com>
2019-01-27 16:36:00 -0500

    add extra stuff

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@ module Main where
 
 import Lib
 
+import Control.Lens
 import Control.Monad.IO.Class
 
 import Web.Scotty
@@ -14,8 +15,6 @@ import Data.Monoid (mconcat)
 
 import Control.Concurrent.MVar
 import Data.IORef
-
-import Network.Wreq
 
 startingState = CSState [Person "djl329" (RankInfo [])] [] [Chore 1]
 
@@ -29,6 +28,14 @@ main = do
             r <- liftIO $ select lock u t state
             case r of
                 Changed (RankInfo ri) -> html $ fromString $ "Your preferences (from favorite to least favorite) are: " ++ show ri
-                AlreadyChosen -> html "You have already selected your chore! So you can't change your preference."
+                AlreadyChosen -> html "You have already selected your chore (or are not choosing chores at all)! So you can't change your preference."
         post "/help" $ html
             "Chore selection help:"
+        post "/list" $ do
+            u <- param "user_name"
+            liftIO $ takeMVar lock
+            s <- liftIO $ readIORef state
+            liftIO $ putMVar lock ()
+            case toListOf (toChoose . traverse . filtered (\x -> view personName x == u)) s of
+                [Person _ (RankInfo ri)] -> html $ fromString $ "Your preferences (from favorite to least favorite) are: " ++ show ri
+                _ -> html "You have already selected your chore (or are not choosing chores at all)! So you can't change your preference."
