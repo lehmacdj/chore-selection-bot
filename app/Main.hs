1e73898b8f7e4e11ae9b740de433929d5946800c
Devin Lehmacher <lehmacdj@gmail.com>
2019-01-27 14:37:15 -0500

    use port 80

--- app/Main.hs
+++ app/Main.hs
@@ -10,6 +10,6 @@ import Data.String (fromString)
 
 import Data.Monoid (mconcat)
 
-main = scotty 3000 $ do
+main = scotty 80 $ do
     get "/select" (request >>= html . fromString . show)
     get "/help" (request >>= html . fromString . show)
